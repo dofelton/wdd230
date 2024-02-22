f47155046d70db5d0f76c97d97991cd33806a178
dofelton <dofelton@gmail.com>
2024-02-22 17:58:21 -0600

    last visited edit

--- chamber/scripts/lastVisited.js
+++ chamber/scripts/lastVisited.js
@@ -3,7 +3,7 @@ let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
 const msInDay = 84600000;
 const todaysDate = Number(new Date());
-let daysSince = ((todaysDate-lastVisit)/msInDay);
+let daysSince = (todaysDate-lastVisit)/msInDay;
 console.log(Date(lastVisit))
 console.log(numVisits)
 console.log(todaysDate)
@@ -11,10 +11,10 @@ console.log(daysSince)
 if (numVisits === 0) {
     visitDisplay.innerHTML = `Welcome! Let us know if you have any questions`;
 }  else {
-    if ((todaysDate - lastVisit) < msInDay) {
+    if (daysSince < 1) {
         visitDisplay.textContent = `Back so soon! Awesome!`;
     } else {
-        visitDisplay.textContent = `You last visited ${daysSince} days ago.`;
+        visitDisplay.textContent = `You last visited ${Math.ceil(daysSince)} days ago.`;
     }
 }
 
