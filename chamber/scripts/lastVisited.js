f439a1df2a4ac7783a00b9c4b5bcecca90557c02
Don Felton <dofelton@gmail.com>
2024-03-05 17:19:17 -0600

    Join page

--- chamber/scripts/lastVisited.js
+++ chamber/scripts/lastVisited.js
@@ -4,10 +4,7 @@ let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
 const msInDay = 84600000;
 const todaysDate = Number(new Date());
 let daysSince = (todaysDate-lastVisit)/msInDay;
-console.log(Date(lastVisit))
-console.log(numVisits)
-console.log(todaysDate)
-console.log(daysSince)
+
 if (numVisits === 0) {
     visitDisplay.innerHTML = `Welcome! Let us know if you have any questions`;
 }  else {
