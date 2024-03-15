c2eb3d5719f72776b79af40f4454bc73f1626e61
Don Felton <dofelton@gmail.com>
2024-03-14 20:36:23 -0500

    weather assignments

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,5 +1,4 @@
 const APIKey = '34fac9256086cfc3e37efb11d46b6199';
-// 0dc2835c1f834a1874a6f13e7e20ccc9
 // Coordinantes for Trier Germany 49.7496659116339, 6.636409678786817
 const lat = 49.7496659116339;
 const lon = 6.636409678786817;
@@ -22,16 +21,25 @@ async function apiFetch() {
     console.log(error);
 }
 }
+apiFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
-    const inconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    // let desc = data.weather[0].icon
-    desc.textContent = `- ${(data.weather[0].description)}`;
-    weatherIcon.setAttribute('src', inconsrc);
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    console.log(iconsrc);
+    let desc = data.weather[0].description;
+    console.log(desc);
+    weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
+    captionDecs.textContent = `${titleCase(desc)}`;
 }
 
-apiFetch();
+function titleCase(string) {
+    string = string.split(' ');
+    for (let i=0; i < string.length; i++) {
+        string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+    }
+    return string.join(' ');
+}
 
 // https://codepen.io/Michael-Bourdet/pen/OJGRyWy
--- scripts/weather.js
+++ scripts/weather.js
@@ -3,6 +3,7 @@ const lat = 32.9029;
 const lon = -96.5639;
 const weatherCard = document.querySelector('.weather');
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.9029&lon=-96.5639&appid=34fac9256086cfc3e37efb11d46b6199&units=imperial"
+console.log('script running');
 
 async function apiFetch() {
     try {
@@ -25,7 +26,6 @@ function displayResults(data) {
     let temp = document.createElement('p');
     let description = document.createElement('p');
     let icon = document.createElement('img');
-    let fcTitle = document.createElement('h3');
 
     temp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
     let desc = data.weather[0].description;
@@ -34,13 +34,9 @@ function displayResults(data) {
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     icon.setAttribute('src', iconsrc);
     icon.setAttribute('alt', desc);
-    fcTitle.textContent = '3-day Forecast';
-
     weatherCard.appendChild(temp);
     weatherCard.appendChild(description);
     weatherCard.appendChild(icon);
-    weatherCard.appendChild(fcTitle);
-
 }
 
 function titleCase(string) {
